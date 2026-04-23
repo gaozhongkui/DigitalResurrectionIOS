0bd799d3217378891e933a1ce39cd5f366776aa0
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-04-23 14:05:26 +0800

    init project

---
+++ DigitalResurrection/DigitalResurrectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalResurrectionApp.swift
+//  DigitalResurrection
+//
+//  Created by gaozhongkui on 2026/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DigitalResurrectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
